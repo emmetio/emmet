9cd2c99d1f5c8a829e0010232e3eea982dfb08c5
Sergey Chikuyonok <serge.che@gmail.com>
2010-10-22 13:32:59 +0400

    Some minor changes

--- plugins/aptana/my_zen_settings.js
+++ plugins/aptana/my_zen_settings.js
@@ -1,8 +1,15 @@
 var my_zen_settings = {
 	'html': {
 		'abbreviations': {
-			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>',
+			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>',
 			'demo': '<div id="demo"></div>'
+		}
+	},
+	
+	'xsl': {
+		'abbreviations': {
+			'im': '<xsl:import href="|"/>',
+			'tx': '<xsl:text></xsl:text>'
 		}
 	}
 };
