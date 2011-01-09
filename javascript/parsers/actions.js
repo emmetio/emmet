6ea56185ed7949b4674758c0851e8413d2c7b398
Sergey Chikuyonok <serge.che@gmail.com>
2011-01-10 00:21:34 +0300

    Reflect CSS value action correctly handles different browser's property names

--- javascript/parsers/actions.js
+++ javascript/parsers/actions.js
@@ -5,6 +5,7 @@
  * @include "../zen_editor.js"
  * @include "parserutils.js"
  * @include "../zen_coding.js"
+ * @include "../zen_actions.js"
  */
  
 /**
@@ -18,7 +19,8 @@ function reflectCSSValue(editor) {
 	var content = String(editor.getContent()),
 		caret_pos = editor.getCaretPos(),
 		sel = editor.getSelectionRange(),
-		css = ParserUtils.extractCSSRule(content, caret_pos);
+		css = ParserUtils.extractCSSRule(content, caret_pos),
+		v;
 		
 	if (!css || caret_pos < css[0] || caret_pos > css[1])
 		// no matching CSS rule or caret outside rule bounds
@@ -30,8 +32,9 @@ function reflectCSSValue(editor) {
 	if (token_ix != -1) {
 		var cur_prop = tokens[token_ix].content,
 			value_token = findValueToken(tokens, token_ix + 1),
-			base_name = cur_prop.replace(/^\s*\-\w+\-/, ''),
+			base_name = getBaseCSSName(cur_prop),
 			re_name = new RegExp('^(?:\\-\\w+\\-)?' + base_name + '$'),
+			re_name = getReflectedCSSName(base_name),
 			values = [];
 			
 		if (!value_token) return false;
@@ -40,28 +43,31 @@ function reflectCSSValue(editor) {
 		for (var i = 0, token, il = tokens.length; i < il; i++) {
 			token = tokens[i];
 			if (token.type == 'identifier' && re_name.test(token.content) && token.content != cur_prop) {
-				var v = findValueToken(tokens, i + 1);
+				v = findValueToken(tokens, i + 1);
 				if (v) 
-					values.push(v);
+					values.push({name: token, value: v});
 			}
 		}
 		
 		// some editors do not provide easy way to replace multiple code 
 		// fragments so we have to squash all replace operations into one
 		if (values.length) {
-			var data = content.substring(values[0].start, values[values.length - 1].end),
-				offset = values[0].start,
+			var data = content.substring(values[0].value.start, values[values.length - 1].value.end),
+				offset = values[0].value.start,
 				value = value_token.content;
 				
 			for (var i = values.length - 1; i >= 0; i--) {
-				data = replaceSubstring(data, values[i].start - offset, values[i].end - offset, value);
+				v = values[i].value;
+				data = replaceSubstring(data, v.start - offset, v.end - offset, 
+					getReflectedValue(cur_prop, value, values[i].name.content, v.content));
+					
 				// also calculate new caret position
-				if (values[i].start < caret_pos) {
-					caret_pos += value.length - values[i].end + values[i].start;
+				if (v.start < caret_pos) {
+					caret_pos += value.length - v.end + v.start;
 				}
 			}
 			
-			editor.replaceContent(data, offset, values[values.length - 1].end, true);
+			editor.replaceContent(data, offset, values[values.length - 1].value.end, true);
 			editor.createSelection(caret_pos, caret_pos + sel.end - sel.start);
 			
 			return true;
@@ -69,6 +75,64 @@ function reflectCSSValue(editor) {
 	}
 	
 	return false;
+}
+
+/**
+ * Removes vendor prefix from CSS property
+ * @param {String} name CSS property
+ * @return {String}
+ */
+function getBaseCSSName(name) {
+	return name.replace(/^\s*\-\w+\-/, '');
+}
+
+/**
+ * Returns regexp that should match reflected CSS property names
+ * @param {String} name Current CSS property name
+ * @return {RegExp}
+ */
+function getReflectedCSSName(name) {
+	name = getBaseCSSName(name);
+	var vendor_prefix = '^(?:\\-\\w+\\-)?', m;
+	
+	if (name == 'opacity' || name == 'filter') {
+		return new RegExp(vendor_prefix + '(?:opacity|filter)$');
+	} else if (m = name.match(/^border-radius-(top|bottom)(left|right)/)) {
+		// Mozilla-style border radius
+		return new RegExp(vendor_prefix + '(?:' + name + '|border-' + m[1] + '-' + m[2] + '-radius)$');
+	} else if (m = name.match(/^border-(top|bottom)-(left|right)-radius/)) { 
+		return new RegExp(vendor_prefix + '(?:' + name + '|border-radius-' + m[1] + m[2] + ')$');
+	}
+	
+	return new RegExp(vendor_prefix + name + '$');
+}
+
+/**
+ * Returns value that should be reflected for <code>ref_name</code> CSS property
+ * from <code>cur_name</code> property. This function is used for special cases,
+ * when the same result must be achieved with different properties for different
+ * browsers. For example: opаcity:0.5; -> filter:alpha(opacity=50);<br><br>
+ * 
+ * This function does value conversion between different CSS properties
+ * 
+ * @param {String} cur_name Current CSS property name
+ * @param {String} cur_value Current CSS property value
+ * @param {String} ref_name Receiver CSS property's name 
+ * @param {String} ref_value Receiver CSS property's value
+ * @return {String} New value for receiver property
+ */
+function getReflectedValue(cur_name, cur_value, ref_name, ref_value) {
+	cur_name = getBaseCSSName(cur_name);
+	ref_name = getBaseCSSName(ref_name);
+	
+	if (cur_name == 'opacity' && ref_name == 'filter') {
+		return ref_value.replace(/opacity=[^)]*/i, 'opacity=' + Math.floor(parseFloat(cur_value) * 100));
+	} else if (cur_name == 'filter' && ref_name == 'opacity') {
+		var m = cur_value.match(/opacity=([^)]*)/i);
+		return m ? prettifyNumber(parseInt(m[1]) / 100) : ref_value;
+	}
+	
+	return cur_value;
 }
 
 /**
