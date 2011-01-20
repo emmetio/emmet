e9fc979f37b916c6a4f5ef9137929025f93bb127
Sergey Chikuyonok <serge.che@gmail.com>
2011-01-21 01:46:29 +0300

    Ported parser actions to Python

--- python/unittest/test_cssex.py
+++ python/unittest/test_cssex.py
@@ -2,23 +2,21 @@
 @author Sergey Chikuyonok (serge.che@gmail.com)
 @link http://chikuyonok.ru
 '''
-from parser import CSSEX
-from parser import XML
+from parser import utils
 
+css = utils.parse_css('.item > #elem {position  :  relative  ;\nborder: 1px solid #000;\nbackground: url(image.png) #fff top left}', 15)
+#for t in css:
+#	print(t)
+
+html = utils.parse_html('<span class="text">', 30)
+#for t in html:
+#	print(t)
+	
+print utils.extract_css_rule('.item > #elem {position  :  relative  ;\nborder: 1px solid #000;\nbackground: url(image.png) #fff top left}', 40, False)
+	
 #tokens = CSSEX.lex('.item > #elem {position  :  relative  ;\nborder: 1px solid #000;\nbackground: url(image.png) #fff top left}')
 #
 #for t in tokens:
 #	print(t)
 	
-html_tokens = XML.parse('<span class="text"></span>\n<em></em>\n<img src=""        alt="" />')
-i = 100
-try:
-	while i > 0:
-		i -= 1
-		t = html_tokens['next']()
-		print('token: %s' % t)
-		
-		if not t:
-			break
-except XML.StopIteration:
-	print('stopped')
+#html_tokens = XML.parse('<span class="text"></span>\n<em></em>\n<img src=""        alt="" />')
