d4dd0b5856b20e8a40a0e47030e9fb66b856c2fe
Sergey Chikuyonok <serge.che@gmail.com>
2011-01-21 00:42:42 +0300

    Ported XML parser to Python

--- python/unittest/test_cssex.py
+++ python/unittest/test_cssex.py
@@ -3,21 +3,22 @@
 @link http://chikuyonok.ru
 '''
 from parser import CSSEX
-from HTMLParser import HTMLParser
+from parser import XML
 
-class MyHTMLParser(HTMLParser):
-	def handle_starttag(self, tag, attrs):
-		line, char = self.getpos()
-		print('got %s at %d, %d' % (self.get_starttag_text(), char, line))
-		
-	def handle_startendtag(self, tag, attrs):
-		line, char = self.getpos()
-		print('got %s at %d, %d' % (self.get_starttag_text(), char, line))
-
-tokens = CSSEX.lex('.item > #elem {position  :  relative  ;\nborder: 1px solid #000;\nbackground: url(image.png) #fff top left}')
-
-for t in tokens:
-	print(t)
+#tokens = CSSEX.lex('.item > #elem {position  :  relative  ;\nborder: 1px solid #000;\nbackground: url(image.png) #fff top left}')
+#
+#for t in tokens:
+#	print(t)
 	
-html = MyHTMLParser()
-html.feed('<span class="text"></span>\n<em></em>\n<img src=""        alt="" />')
+html_tokens = XML.parse('<span class="text"></span>\n<em></em>\n<img src=""        alt="" />')
+i = 100
+try:
+	while i > 0:
+		i -= 1
+		t = html_tokens['next']()
+		print('token: %s' % t)
+		
+		if not t:
+			break
+except XML.StopIteration:
+	print('stopped')
