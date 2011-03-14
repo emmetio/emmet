407e97cdcc71e07d1b74f0856ec5a784b9d27c7b
thorn0 <thorn.mailbox@gmail.com>
2011-03-14 16:51:24 +0200

    ASP.NET filter: profile awareness, fix for self-closing tags

--- javascript/filters/asp.js
+++ javascript/filters/asp.js
@@ -4,14 +4,16 @@
  */
 (function(){
 
-	function process(tree) {
+	function process(tree, profile) {
+		var attr_quote = profile.attr_quotes == 'single' ? "'" : '"';
+		var attr_name = profile.attr_case == 'upper' ? 'RUNAT' : profile.attr_case == 'lower' ? 'runat' : 'RunAt';
 		for (var i = 0, il = tree.children.length; i < il; i++) {
 			var item = tree.children[i];
-			if (item.type == 'tag' && item.name && item.name.toLowerCase().indexOf(':') != -1 && item.start.toLowerCase().indexOf('runat') == -1)
-				item.start = item.start.replace(/>/, ' runat="server">');
-			process(item);
+			if (item.type == 'tag' && item.name && item.name.indexOf(':') != -1 && !/\srunat\b/i.test(item.start))
+				item.start = item.start.replace(/(\/)?>/, ' ' + attr_name + '=' + attr_quote + 'server' + attr_quote + '$1>');
+			process(item, profile);
 		}
 	}
-	
+
 	zen_coding.registerFilter('asp', process);
 })();
