67612c872e62220e51c37d91e0d7a75e59a692f0
Sergey Chikuyonok <serge.che@gmail.com>
2012-07-09 23:20:15 +0400

    Fixed unit-tests

--- javascript/unittest/tests/actions.js
+++ javascript/unittest/tests/actions.js
@@ -4,23 +4,10 @@
 		actions.run(name, editorStub);
 	};
 	
-	var caret = zen_coding.require('utils').getCaretPlaceholder();
-	
 	module('Actions');
-	QUnit.moduleStart(function(obj) {
-		if (obj.name == 'Actions') {
-			zen_coding.require('utils').setCaretPlaceholder('|');
-		}
-	});
-	
-	QUnit.moduleDone(function(obj) {
-		if (obj.name == 'Actions') {
-			zen_coding.require('utils').setCaretPlaceholder(caret);
-		}
-	});
 	
 	test('Expand Abbreviatoin', function() {
-		var content = 'a>b| test';
+		var content = 'a>b${0} test';
 		editorStub.replaceContent(content);
 		
 		run('expand_abbreviation');
@@ -29,7 +16,7 @@
 	});
 	
 	test('Move to edit points', function() {
-		var content = '|<a href="">\n\t<b></b>\n\t\n</a>';
+		var content = '${0}<a href="">\n\t<b></b>\n\t\n</a>';
 		editorStub.replaceContent(content);
 		
 		run('next_edit_point');
@@ -60,27 +47,27 @@
 	});
 	
 	test('Evaluate math expression', function() {
-		var content = 'hello 2+4| world';
+		var content = 'hello 2+4${0} world';
 		editorStub.replaceContent(content);
 		run('evaluate_math_expression');
 		equal(editorStub.getContent(), 'hello 6 world', 'Evaluated expression in ' + content);
 		
-		editorStub.replaceContent(content = 'hello 2*4| world');
+		editorStub.replaceContent(content = 'hello 2*4${0} world');
 		run('evaluate_math_expression');
 		equal(editorStub.getContent(), 'hello 8 world', 'Evaluated expression in ' + content);
 		
-		editorStub.replaceContent(content = 'hello 12*4| world');
+		editorStub.replaceContent(content = 'hello 12*4${0} world');
 		editorStub.createSelection(7, 10);
 		run('evaluate_math_expression');
 		equal(editorStub.getContent(), 'hello 18 world', 'Evaluated expression in ' + content);
 		
-		editorStub.replaceContent(zen_coding.require('utils').escapeText(content = 'hello 11\\2| world'));
+		editorStub.replaceContent(content = 'hello 11\\\\2${0} world');
 		run('evaluate_math_expression');
 		equal(editorStub.getContent(), 'hello 6 world', 'Evaluated expression in ' + content);
 	});
 	
 	test('Increment/decrement', function() {
-		editorStub.replaceContent('a 1| b');
+		editorStub.replaceContent('a 1${0} b');
 		run('increment_number_by_1');
 		equal(editorStub.getContent(), 'a 2 b', 'Incremented number by 1');
 		
@@ -100,14 +87,14 @@
 		run('decrement_number_by_1');
 		equal(editorStub.getContent(), 'a 1 b', 'Decremented number by 1');
 		
-		editorStub.replaceContent('index|1.html');
+		editorStub.replaceContent('index${0}1.html');
 		run('increment_number_by_1');
 		equal(editorStub.getContent(), 'index2.html', 'Incremented number in filename by 1');
 		
 	});
 	
 	test('Formatted line breaks', function() {
-		editorStub.replaceContent('<a>|</a>');
+		editorStub.replaceContent('<a>${0}</a>');
 		run('insert_formatted_line_break');
 		equal(editorStub.getContent(), '<a>\n\t\n</a>', 'Inserted linebreak between HTML tags');
 		
@@ -116,15 +103,15 @@
 		
 		editorStub.setSyntax('css');
 		
-		editorStub.replaceContent('.a {|');
+		editorStub.replaceContent('.a {${0}');
 		run('insert_formatted_line_break');
 		equal(editorStub.getContent(), '.a {\n\t\n}', 'Inserted linebreak after CSS open brace');
 		
-		editorStub.replaceContent('.a {|}');
+		editorStub.replaceContent('.a {${0}}');
 		run('insert_formatted_line_break');
 		equal(editorStub.getContent(), '.a {\n\t\n}', 'Inserted linebreak in CSS braces');
 		
-		editorStub.replaceContent('.a |{}');
+		editorStub.replaceContent('.a ${0}{}');
 		run('insert_formatted_line_break');
 		equal(editorStub.getContent(), '.a \n{}', 'Inserted linebreak in CSS braces');
 		
@@ -132,7 +119,7 @@
 	});
 	
 	test('Match pair', function() {
-		editorStub.replaceContent('Lorem <a><b>ip|sum</b></a> dolor sit amet');
+		editorStub.replaceContent('Lorem <a><b>ip${0}sum</b></a> dolor sit amet');
 		
 		run('match_pair_outward');
 		deepEqual(editorStub.getSelectionRange(), {start: 12, end: 17}, 'Matched tag content');
@@ -151,7 +138,7 @@
 	});
 	
 	test('Merge lines', function() {
-		editorStub.replaceContent('<b>\n\n|\n\n</b>');
+		editorStub.replaceContent('<b>\n\n${0}\n\n</b>');
 		
 		run('merge_lines');
 		equal(editorStub.getContent(), '<b></b>', 'Automatched and merged tag content');
@@ -165,16 +152,16 @@
 	test('Reflect CSS Value', function() {
 		editorStub.setSyntax('css');
 		
-		editorStub.replaceContent('a {p:1; -a-p:12|; -b-p:1; x:1;}');
+		editorStub.replaceContent('a {p:1; -a-p:12${0}; -b-p:1; x:1;}');
 		run('reflect_css_value');
 		equal(editorStub.getContent(), 'a {p:12; -a-p:12; -b-p:12; x:1;}', 'Reflected value of -a-p');
 		equal(editorStub.getCaretPos(), 16, 'Correctly adjusted caret position');
 		
-		editorStub.replaceContent('a {opacity: 0.5|; filter: alpha(opacity=60)}');
+		editorStub.replaceContent('a {opacity: 0.5${0}; filter: alpha(opacity=60)}');
 		run('reflect_css_value');
 		equal(editorStub.getContent(), 'a {opacity: 0.5; filter: alpha(opacity=50)}', 'Custom reflection for opacity');
 		
-		editorStub.replaceContent('a {border-top-left-radius: 10px|; -moz-border-radius-topleft: 5px;}');
+		editorStub.replaceContent('a {border-top-left-radius: 10px${0}; -moz-border-radius-topleft: 5px;}');
 		run('reflect_css_value');
 		equal(editorStub.getContent(), 'a {border-top-left-radius: 10px; -moz-border-radius-topleft: 10px;}', 'Custom reflection for border radius');
 		
@@ -182,11 +169,11 @@
 	});
 	
 	test('Remove tag', function() {
-		editorStub.replaceContent('hello\n<di|v>\n\t<span>world</span>\n</div>');
+		editorStub.replaceContent('hello\n<di${0}v>\n\t<span>world</span>\n</div>');
 		run('remove_tag');
 		equal(editorStub.getContent(), 'hello\n<span>world</span>', 'Removed <div> tag');
 		
-		editorStub.replaceContent('hello <img| src="" alt="" /> world');
+		editorStub.replaceContent('hello <img${0} src="" alt="" /> world');
 		run('remove_tag');
 		equal(editorStub.getContent(), 'hello  world', 'Removed <img /> tag');
 	});
@@ -194,7 +181,7 @@
 	test('Select prev/next item (CSS)', function() {
 		editorStub.setSyntax('css');
 		
-		editorStub.replaceContent('|a {color: red; border: 1px solid black;}');
+		editorStub.replaceContent('${0}a {color: red; border: 1px solid black;}');
 		run('select_next_item');
 		deepEqual(editorStub.getSelectionRange(), {start: 0, end: 1}, 'Selector matched');
 		
@@ -259,7 +246,7 @@
 	});
 	
 	test('Select prev/next item (HTML)', function() {
-		editorStub.replaceContent('|<div class="hello world" b=1>text</div> <b class="large">text 2</b>');
+		editorStub.replaceContent('${0}<div class="hello world" b=1>text</div> <b class="large">text 2</b>');
 		
 		run('select_next_item');
 		deepEqual(editorStub.getSelectionRange(), {start: 1, end: 4}, 'Matched "div" tag name');
@@ -330,13 +317,13 @@
 	});
 	
 	test('Select current line', function() {
-		editorStub.replaceContent('Lorem\nipsum dol|or sit\namet');
+		editorStub.replaceContent('Lorem\nipsum dol${0}or sit\namet');
 		run('select_line');
 		equal(editorStub.getSelection(), 'ipsum dolor sit', 'Selected line');
 	});
 	
 	test('Split/join current tag', function() {
-		editorStub.replaceContent('<span class="|sample"></span>');
+		editorStub.replaceContent('<span class="${0}sample"></span>');
 		
 		run('split_join_tag');
 		equal(editorStub.getContent(), '<span class="sample" />', 'Inline tag is joined');
@@ -347,7 +334,7 @@
 		
 		var oldProfile = editorStub.getProfileName();
 		editorStub.setProfileName('xml');
-		editorStub.replaceContent('<div class="|sample"></div>');
+		editorStub.replaceContent('<div class="${0}sample"></div>');
 		
 		run('split_join_tag');
 		equal(editorStub.getContent(), '<div class="sample"/>', 'XML tag is joined');
@@ -358,7 +345,7 @@
 	});
 
 	test('Toggle comment (HTML)', function() {
-		editorStub.replaceContent('hello <sp|an>world</span>');
+		editorStub.replaceContent('hello <sp${0}an>world</span>');
 		
 		run('toggle_comment');
 		equal(editorStub.getContent(), 'hello <!-- <span>world</span> -->', 'Added comment to <span> element');
@@ -370,7 +357,7 @@
 	
 	test('Toggle comment (CSS)', function() {
 		editorStub.setSyntax('css');
-		editorStub.replaceContent('a {color: red; font-weight:| bold;} b {color: black;}');
+		editorStub.replaceContent('a {color: red; font-weight:${0} bold;} b {color: black;}');
 		
 		run('toggle_comment');
 		equal(editorStub.getContent(), 'a {color: red; /* font-weight: bold; */} b {color: black;}', 'Added comment to font-weight property');
@@ -387,15 +374,15 @@
 	});
 	
 	test('Update image size (HTML)', function() {
-		editorStub.replaceContent('<img| src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" alt="" />');
+		editorStub.replaceContent('<img${0} src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" alt="" />');
 		run('update_image_size');
 		equal(editorStub.getContent(), '<img src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" alt="" width="3" height="2" />', 'Added "width" and "height" attributes into <img> tag');
 		
-		editorStub.replaceContent('<img| src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="100" height="100" alt="" />');
+		editorStub.replaceContent('<img${0} src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="100" height="100" alt="" />');
 		run('update_image_size');
 		equal(editorStub.getContent(), '<img src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="3" height="2" alt="" />', 'Updated existing attributes');
 		
-		editorStub.replaceContent('<img| src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="100" alt="" />');
+		editorStub.replaceContent('<img${0} src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="100" alt="" />');
 		run('update_image_size');
 		equal(editorStub.getContent(), '<img src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="3" height="2" alt="" />', 'Updated "width" attribute and added "height" attribute');
 	});
@@ -403,15 +390,15 @@
 	test('Update image size (CSS)', function() {
 		editorStub.setSyntax('css');
 		
-		editorStub.replaceContent('.test {\n\tbackgrou|nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n}');
+		editorStub.replaceContent('.test {\n\tbackgrou${0}nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n}');
 		run('update_image_size');
 		equal(editorStub.getContent(), '.test {\n\tbackground: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 3px;\n\theight: 2px;\n}', 'Added "width" and "height" properties');
 		
-		editorStub.replaceContent('.test {\n\tbackgrou|nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 100px;\n\theight: 100px;\n}');
+		editorStub.replaceContent('.test {\n\tbackgrou${0}nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 100px;\n\theight: 100px;\n}');
 		run('update_image_size');
 		equal(editorStub.getContent(), '.test {\n\tbackground: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 3px;\n\theight: 2px;\n}', 'Updated "width" and "height" properties');
 		
-		editorStub.replaceContent('.test {\n\tbackgrou|nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 100px;\n}');
+		editorStub.replaceContent('.test {\n\tbackgrou${0}nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 100px;\n}');
 		run('update_image_size');
 		equal(editorStub.getContent(), '.test {\n\tbackground: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 3px;\n\theight: 2px;\n}', 'Updated "width" property and added "height"');
 		
@@ -419,7 +406,7 @@
 	});
 	
 	test('Wrap With Abbreviation', function() {
-		editorStub.replaceContent('<br| />');
+		editorStub.replaceContent('<br${0} />');
 		editorStub.setPromptOutput('ul>li');
 		run('wrap_with_abbreviation');
 		equal(editorStub.getContent(), '<ul>\n\t<li>\n\t\t<br />\n\t</li>\n</ul>', 'Wrapped with `ul>li`');
@@ -451,6 +438,6 @@
 		equal(wrap('li[title=$# class=item-$#]*>em{Label: $#}', 'one\ntwo\nthree'), '<li title="one" class="item-one"><em>Label: one</em></li><li title="two" class="item-two"><em>Label: two</em></li><li title="three" class="item-three"><em>Label: three</em></li>');
 		
 		// wrap with snippet
-		equal(wrap('cc:ie', 'hello world'), '<!--[if IE]>\n\thello world|\n<![endif]-->');
+		equal(wrap('cc:ie', 'hello world'), '<!--[if IE]>\n\thello world${0}\n<![endif]-->');
 	});
 })();
