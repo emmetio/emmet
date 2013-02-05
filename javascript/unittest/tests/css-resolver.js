0d128f0f29c13e52b2b422cd17a2bca1ded3f200
Sergey Chikuyonok <serge.che@gmail.com>
2013-02-05 18:19:29 +0400

    Fixed #144

--- javascript/unittest/tests/css-resolver.js
+++ javascript/unittest/tests/css-resolver.js
@@ -68,6 +68,7 @@ test('Abbreviation expanding', function() {
 	equal(css.expandToSnippet('something'), 'something: ${1};', 'Expanded unknown property');
 	equal(css.expandToSnippet('-bxsh'), '-webkit-box-shadow: ${1:hoff} ${2:voff} ${3:radius} ${4:color};\n-moz-box-shadow: ${1:hoff} ${2:voff} ${3:radius} ${4:color};\nbox-shadow: ${1:hoff} ${2:voff} ${3:radius} ${4:color};', 'Expanded property with multiple tabstops');
 	equal(css.expandToSnippet('bd1#0solid'), 'border: 1px #000 solid;', 'Expanded "bd1#0solid"');
+	equal(css.expandToSnippet('bd1-s-blue'), 'border: 1px solid blue;', 'Expanded "bd1-s-blue"');
 	equal(css.expandToSnippet('c#3d3d3d'), 'color: #3d3d3d;', 'Expanded "c#3d3d3d"');
 	equal(css.expandToSnippet('c#d3d3d3'), 'color: #d3d3d3;', 'Expanded "c#d3d3d3"');
 	equal(css.expandToSnippet('c#'), 'color: #000;', 'Expanded "c#"');
