9e6d9e3f5c4fed0dfa69c701ab631b04d27adabe
Sergey Chikuyonok <serge.che@gmail.com>
2013-12-27 12:57:06 +0400

    Finished LESS selector resolver

--- test/resolver/less.js
+++ test/resolver/less.js
@@ -8,11 +8,29 @@ describe('LESS selector resolver', function() {
 	var lessFile = fs.readFileSync(path.join(__dirname, '../preprocessors/resolver.less'), {encoding: 'utf8'});
 	it('should work', function() {
 		var tree = cssSections.sectionTree(lessFile);
-
 		var resolved = lessResolver.resolve(tree);
+		var expectedResult = [
+			['.bordered'],
+			['#header'],
+			['#header .navigation'],
+			['#header .logo'],
+			['#header .logo:hover'],
+			['.one'],
+			['@media (width: 400px)', '.one'],
+			['@media (width: 400px) and print and color', '.one'],
+			['div, p'],
+			['p'],
+			['.a.b.test, .test.c, .a.b.replacement, .replacement.c'],
+			['.test, .replacement'],
+			['.test:hover, .replacement:hover'],
+			['.replacement'],
+			['#bundle'],
+			['#header a']
+		];
 
-		resolved.forEach(function(item) {
-			console.log(item.path);
+		resolved.forEach(function(item, i) {
+			assert.deepEqual(item.path, expectedResult[i]);
+			console.log('[%s] points to [%s]', item.path.join(', '), item.node.name());
 		});
 	});
 });
