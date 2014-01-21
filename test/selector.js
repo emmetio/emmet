85b3c89157f42cebf0daf89a9936b0bf790bc13d
Sergey Chikuyonok <serge.che@gmail.com>
2014-01-22 01:44:42 +0400

    Updated selector module

--- test/selector.js
+++ test/selector.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var sel = require('../lib/utils/selector');
+var sel = require('../lib/assets/selector');
 describe('Selector utils', function() {
 	it('should extract rules from selector', function() {
 		assert.deepEqual(sel.rules('a, b'), ['a', 'b']);
@@ -9,48 +9,45 @@ describe('Selector utils', function() {
 	});
 
 	it('should extract parts from rule', function() {
-		assert.deepEqual(sel.parts('a b'), ['a', 'b']);
-		assert.deepEqual(sel.parts('a + b'), ['a', 'b']);
-		assert.deepEqual(sel.parts('a>b'), ['a', 'b']);
-		assert.deepEqual(sel.parts('#nav > li[ title ] a + span'), ['#nav', 'li[ title ]', 'a', 'span']);
+		var parts = function(str) {
+			return sel.parts(str).map(function(p) {
+				return p.valueOf();
+			});
+		};
+
+		assert.deepEqual(parts('a b'), ['a', 'b']);
+		assert.deepEqual(parts('a + b'), ['a', 'b']);
+		assert.deepEqual(parts('a>b'), ['a', 'b']);
+		assert.deepEqual(parts('#nav > li[ title ] a + span'), ['#nav', 'li[title]', 'a', 'span']);
 	});
 
 	it('should parse selector token', function() {
-		assert.deepEqual(sel.parse('ul'), {
-			'name': 'ul'
-		});
+		var parse = function(str) {
+			return sel.parts(str)[0]._fragments;
+		};
 
-		assert.deepEqual(sel.parse('ul#nav.item1.item2'), {
-			'name': 'ul',
-			'#': ['nav'],
-			'.': ['item1', 'item2']
-		});
+		assert.deepEqual(parse('ul'), ['ul']);
+		assert.deepEqual(parse('ul#nav.item1.item2'), 
+			['ul', '#nav', '.item1', '.item2']);
+		assert.deepEqual(parse('ul#nav.item1[title="test"][ data-attr ].item2'), 
+			['ul', '#nav', '.item1', '[title="test"]', '[data-attr]', '.item2']);
 
-		assert.deepEqual(sel.parse('ul#nav.item1[title="test" data-attr].item2'), {
-			'name': 'ul',
-			'#': ['nav'],
-			'.': ['item1', 'item2'],
-			'attrs': {
-				'title': 'test',
-				'data-attr': null
-			}
-		});
-
-		assert.deepEqual(sel.parse('a:hover'), {
-			'name': 'a',
-			':': ['hover']
-		});
+		assert.deepEqual(parse('a:hover'), ['a', ':hover']);
 	});
 
-	it.only('should compare selectors for extention', function() {
-		assert(sel.canExtend('.test', '.test'));
-		assert(sel.canExtend('.test', '.test.test2'));
-		assert(!sel.canExtend('.test', '.test2'));
-		assert(sel.canExtend(':hover', 'a:hover'));
-		assert(sel.canExtend('a', 'a:hover'));
-		assert(!sel.canExtend('b:hover', 'a:hover'));
-		assert(sel.canExtend(':hover', 'a.item:hover'));
-		assert(sel.canExtend('#id', '#id.item:hover'));
-		assert(!sel.canExtend('#id', '.item:hover'));
+	it('should compare selectors for extention', function() {
+		var canExtend = function(target, selector) {
+			return sel.create(target).canExtend(selector);
+		};
+
+		assert(canExtend('.test', '.test'));
+		assert(canExtend('.test', '.test.test2'));
+		assert(!canExtend('.test', '.test2'));
+		assert(canExtend(':hover', 'a:hover'));
+		assert(canExtend('a', 'a:hover'));
+		assert(!canExtend('b:hover', 'a:hover'));
+		assert(canExtend(':hover', 'a.item:hover'));
+		assert(canExtend('#id', '#id.item:hover'));
+		assert(!canExtend('#id', '.item:hover'));
 	});
 });
