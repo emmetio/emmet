7aa7e4a69a341cf545878e53974e469062b35d64
Sergey Chikuyonok <serge.che@gmail.com>
2016-06-28 23:53:55 +0300

    Switched to extenal Can I Use database
    Minor refactoring

--- bundles/caniuse.js
+++ bundles/caniuse.js
@@ -2,6 +2,6 @@
  * Bundler, used in builder script to statically
  * include optimized caniuse.json into bundle
  */
+var db = require('caniuse-db/data.json');
 var ciu = require('../lib/assets/caniuse');
-var db = require('../lib/caniuse.json');
 ciu.load(db, true);
