92e49cb5c79b3d3588a1b92d5eb993b74113488a
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-07-04 18:36:59 +0300

    Callback for `locateFile` function and throw error if file not exists

--- test/actions/base64.js
+++ test/actions/base64.js
@@ -10,4 +10,14 @@ describe('Base64 encoder/decoder', function() {
 		action.encodeDecodeDataUrlAction(editor);
 		assert.equal(editor.getContent(), '<img src="data:image/gif;base64,R0lGODdhAQABAIAAAAAAAAAAACH5BAEAAAEALAAAAAABAAEAAAICTAEAOw==" />');
 	});
+
+	it('should return an error if the file does not exist', function() {
+		editor.replaceContent('<img src="${0}error.gif" />');
+
+		try {
+			action.encodeDecodeDataUrlAction(editor);
+		} catch (err) {
+			assert.equal(err, 'Can\'t find error.gif file');
+		}
+	});
 });
