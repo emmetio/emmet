65c2de65bfaf78dcb581bb658eab7d3fa6dbb5af
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-07-05 19:04:52 +0300

    Callback API (#456)
    * Trim whitespace
    
    * Callback for `locateFile` function and throw error if file not exists
    
    * Add callback for create path & save

--- test/actions/updateImageSize.js
+++ test/actions/updateImageSize.js
@@ -14,11 +14,11 @@ describe('Update Image Size action', function() {
 		// insert attributes
 		run('<img${0} src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" alt="" />');
 		assert.equal(editor.getContent(), '<img src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" alt="" width="3" height="2" />');
-		
+
 		// update existing attributes
 		run('<img${0} src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="100" height="100" alt="" />');
 		assert.equal(editor.getContent(), '<img src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="3" height="2" alt="" />');
-		
+
 		// update existing attribute and insert new one
 		run('<img${0} src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="100" alt="" />');
 		assert.equal(editor.getContent(), '<img src="data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==" width="3" height="2" alt="" />');
@@ -26,7 +26,7 @@ describe('Update Image Size action', function() {
 
 	it('should work for CSS', function() {
 		editor.setSyntax('css');
-		
+
 		// Added "width" and "height" properties
 		run('.test {\n\tbackgrou${0}nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n}');
 		assert.equal(editor.getContent(), '.test {\n\tbackground: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 3px;\n\theight: 2px;\n}');
@@ -34,11 +34,19 @@ describe('Update Image Size action', function() {
 		// Updated "width" and "height" properties		
 		run('.test {\n\tbackgrou${0}nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 100px;\n\theight: 100px;\n}');
 		assert.equal(editor.getContent(), '.test {\n\tbackground: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 3px;\n\theight: 2px;\n}');
-		
+
 		// Updated "width" property and added "height"
 		run('.test {\n\tbackgrou${0}nd: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 100px;\n}');
 		assert.equal(editor.getContent(), '.test {\n\tbackground: url(data:image/gif;base64,R0lGODlhAwACAHAAACH5BAUAAAAALAAAAAADAAIAQAIChF8AOw==);\n\twidth: 3px;\n\theight: 2px;\n}');
-		
+
 		editor.setSyntax('html');
+	});
+
+	it('should return an error if the file does not exist', function() {
+		try {
+			run('<img src="${0}error.gif" />');
+		} catch (err) {
+			assert.equal(err, 'Can\'t find error.gif file');
+		}
 	});
 });
