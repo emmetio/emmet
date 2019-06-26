7a8323bda6f3e19d298fd36e0d1638d572a298ad
Sergey Chikuyonok <serge.che@gmail.com>
2019-06-26 18:38:43 +0300

    Prepare CSS abbreviation parser for publishing

--- packages/css-abbreviation/rollup.config.js
+++ packages/css-abbreviation/rollup.config.js
@@ -1,12 +1,18 @@
+import typescript from 'rollup-plugin-typescript2';
+
 export default {
-	input: './index.js',
-	external: [
-		'@emmetio/stream-reader',
-		'@emmetio/stream-reader-utils',
-		'@emmetio/node'
-	],
-	output: [
-		{ format: 'cjs', file: 'dist/css-abbreviation.cjs.js' },
-		{ format: 'es',  file: 'dist/css-abbreviation.es.js' }
-	]
+    input: './src/index.ts',
+    external: [
+        '@emmetio/stream-reader',
+        '@emmetio/stream-reader/utils'
+    ],
+    plugins: [typescript({
+        tsconfigOverride: {
+            compilerOptions: { module: 'esnext' }
+        }
+    })],
+    output: [
+        { format: 'cjs', file: 'dist/css-abbreviation.cjs.js' },
+        { format: 'es',  file: 'dist/css-abbreviation.es.js' }
+    ]
 };
